b95cf82adae10ade60eec5d0c5b120d292a6d780
jadejapratap <pratap.kadachha@corewaysolution.com>
2023-08-16 16:02:27 +0530

    analytics

--- src/component/Linechart.js
+++ src/component/Linechart.js
@@ -1,4 +1,4 @@
-import {PolarisVizProvider, SparkBarChart, SparkLineChart, BarChart, LineChart} from '@shopify/polaris-viz';
+import {PolarisVizProvider, SparkBarChart, SparkLineChart, BarChart, LineChart, BarChart} from '@shopify/polaris-viz';
 import '@shopify/polaris-viz/build/esm/styles.css';
 
 import app from "../fire-config.js";
@@ -24,57 +24,93 @@ import { useState, useCallback, useEffect, useRef } from 'react';
   }, [shopid]); 
 
 
+  const dateRange = (startDate, endDate, steps = 1) => {
+    const dateArray = [];
+    let currentDate = new Date(startDate);
+    while (currentDate <= new Date(endDate)) {
+      dateArray.push(new Date(currentDate).toDateString());
+      currentDate.setUTCDate(currentDate.getUTCDate() + steps);
+    }
+    return dateArray;
+  }
+  
+  const dates = dateRange('2023-08-1', new Date());
+  var notifiarray = [
+                  { value: 100, key: "Tue Aug 01 2023" },
+                  { value: 99, key: "Wed Aug 02 2023" },
+                  { value: 1000, key: "Tue Aug 15 2023" },
+   
+                ];
+  const arrayColumn = (arr, n) => arr.map(x => x[n]);
+  var comparray = arrayColumn(notifiarray, "key");
+  dates.map(function (val, index) {
+     comparray.indexOf(val) === -1 ? notifiarray.push({value: 0, key: val}) : console.log(val+"This item already exists");
+  });
+  
+  notifiarray.sort(function(a, b) {
+    var keyA = new Date(a.key),
+      keyB = new Date(b.key);
+    if (keyA < keyB) return -1;
+    if (keyA > keyB) return 1;
+    return 0;
+  });
+  
+  
+                
+                console.log(notifiarray);
+
+
 return(
 
 <div style={{height: '300px'}} >
 <PolarisVizProvider>
-  <LineChart
+  <BarChart
+
   theme='Light'
  data={[
-      {
-        name: "Notifications",
-        data: Notifications
-      },
-      {
-        name: "Notifications sent",
-        data: [
-          {
-            key: 0,
-            value: 10
-          },
-          {
-            key: 1,
-            value: 20
-          },
-          {
-            key: 2,
-            value: 30
-          }
-        ]
-      },
-      {
-        name: "Order Value",
-        data: [
-          {
-            key: 0,
-            value: 250
-          },
-          {
-            key: 1,
-            value: 350
-          },
-          {
-            key: 2,
-            value: 390
-          }
-        ]
-      }
+  {
+    name: "Notifications",
+    data: [
+      { value: 100, key: "2020-04-01T12:00:00" },
+      { value: 99, key: "2020-04-02T12:00:00" },
+      { value: 1000, key: "2020-04-03T12:00:00" },
+      { value: 2, key: "2020-04-04T12:00:00" },
+      { value: 22, key: "2020-04-05T12:00:00" },
+      { value: 6, key: "2020-04-06T12:00:00" },
+      { value: 5, key: "2020-04-07T12:00:00" }
+    ]
+  },
+  {
+    name: "Notifications sent",
+    data: [
+      { value: 90, key: "2020-04-01T12:00:00" },
+      { value: 80, key: "2020-04-02T12:00:00" },
+      { value: 900, key: "2020-04-03T12:00:00" },
+      { value: 1, key: "2020-04-04T12:00:00" },
+      { value: 20, key: "2020-04-05T12:00:00" },
+      { value: 3, key: "2020-04-06T12:00:00" },
+      { value: 4, key: "2020-04-07T12:00:00" }
+    ]
+  },
+  {
+    name: "Order Value",
+    data: [
+      { value: 80, key: "2020-04-01T12:00:00" },
+      { value: 70, key: "2020-04-02T12:00:00" },
+      { value: 800, key: "2020-04-03T12:00:00" },
+      { value: 3, key: "2020-04-04T12:00:00" },
+      { value: 10, key: "2020-04-05T12:00:00" },
+      { value: 1, key: "2020-04-06T12:00:00" },
+      { value: 50, key: "2020-04-07T12:00:00" }
+    ]
+  }
     ]}
     // dimensions={{
     //   height: 1000,
     //   width: 1000
     // }}
     isAnimated
+    state="Success"
   />
 
 </PolarisVizProvider>
